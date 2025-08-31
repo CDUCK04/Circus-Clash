5559e1e43f5bd3df692ff6309cbe6fba42926657
Teimurazi Bakuradze <168293265+CDUCK04@users.noreply.github.com>
2025-08-31 13:10:58 -0400

    combat changes

--- Circus-Clash/Assets/Scripts/Projectile2D.cs
+++ Circus-Clash/Assets/Scripts/Projectile2D.cs
@@ -7,15 +7,27 @@ public class Projectile2D : MonoBehaviour
 
     int damage;
     Vector2 velocity;
+    string bulletName;
 
     public void Init(int dmg, Vector2 vel) { damage = dmg; velocity = vel; }
-    void Start() => Destroy(gameObject, lifeTime);
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+        bulletName = this.name;
+    }
 
     void Update() => transform.Translate(velocity * Time.deltaTime);
 
     void OnTriggerEnter2D(Collider2D other)
     {
         var hp = other.GetComponentInParent<CircusClash.Troops.Combat.UnitHealth>();
-        if (hp != null && !hp.IsDead) { hp.TakeDamage(damage); Destroy(gameObject); }
+
+        if (hp == null || hp.IsDead) return;
+
+        if (other.CompareTag(this.tag)) return;
+
+        hp.TakeDamage(damage);
+        Destroy(gameObject);
     }
 }
